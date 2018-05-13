8d42daddcf65b730774aecef8c36bd7a8bdfcb1a
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2018-05-13 14:43:09 +0200

    Fix invisible buttons

--- Clean Me/BlueButton.swift
+++ Clean Me/BlueButton.swift
@@ -12,13 +12,12 @@ class BlueButton: NSButton {
 
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
+        
+        self.layer?.backgroundColor = CGColor.init(red: 45.0/255, green: 135.0/255, blue: 250.0/255, alpha: 1)
+        self.layer?.cornerRadius = 15.0
     }
     
     override func awakeFromNib() {
-        //GUI
-        self.layer?.backgroundColor = CGColor.init(red: 45.0/255, green: 135.0/255, blue: 250.0/255, alpha: 1)
-        self.layer?.cornerRadius = 15.0
-        
         //text
         let style = NSMutableParagraphStyle()
         style.alignment = .center
--- Clean Me/GrayButton.swift
+++ Clean Me/GrayButton.swift
@@ -12,10 +12,7 @@ class GrayButton: NSButton {
 
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
-    }
-    
-    override func awakeFromNib() {
-        //GUI
+
         self.layer?.backgroundColor = CGColor.init(red: 0.3, green: 0.3, blue: 0.3, alpha: 0.3)
         self.layer?.cornerRadius = 15.0
     }
